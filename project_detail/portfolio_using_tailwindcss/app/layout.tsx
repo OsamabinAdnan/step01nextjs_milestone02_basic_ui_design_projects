53172579bc33cc41bf3a16b075221954173efa7e
OsamabinAdnan <binadnanosama@gmail.com>
2024-11-18 03:53:45 +0500

    commit after changes

--- project_detail/portfolio_using_tailwindcss/app/layout.tsx
+++ project_detail/portfolio_using_tailwindcss/app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import { inter } from "./ui/font";
 import { montserrat } from "./ui/font";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Portfolio | OsamabinAdnan",
--- project_detail/portfolio_using_tailwindcss/app/ui/font.ts
+++ project_detail/portfolio_using_tailwindcss/app/ui/font.ts
@@ -1,7 +1,8 @@
+
 import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-import {Montserrat} from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
