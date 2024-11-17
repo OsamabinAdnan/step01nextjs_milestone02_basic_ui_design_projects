97e8ffdcb4e384139583cce47b581d2b258d9677
OsamabinAdnan <binadnanosama@gmail.com>
2024-11-18 03:35:13 +0500

    commit after second deloyment failed

--- project_detail/portfolio_using_customcss/app/ui/font.ts
+++ project_detail/portfolio_using_customcss/app/ui/font.ts
@@ -2,6 +2,6 @@ import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-import {Montserrat} from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
 export const montserrat = Montserrat({ subsets: ['latin'] });
